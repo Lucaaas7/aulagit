2dbaa9336bba2464effc7a71c0c85951f9b30090
LucasEduardo <lucas.cravo@hotmail.com>
2021-08-31 14:05:09 -0300

    arrumando tudo

--- aula02/Exemplos/Exemplo01.java
+++ aula02/Exemplos/Exemplo01.java
@@ -1,3 +1,4 @@
+package Exemplos;
 /**
  * Exemplo01
  */
--- aula02/Exemplos/Exemplo02.java
+++ aula02/Exemplos/Exemplo02.java
@@ -1,3 +1,4 @@
+package Exemplos;
 public class Exemplo02 {
     public static void main(String[] args) {
         System.out.println("ZzZzZzZz");
