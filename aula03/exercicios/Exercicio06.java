105410111800129b8380ff057375c67ac5f0c446
LucasEduardo <lucas.cravo@hotmail.com>
2021-09-01 21:21:32 -0300

    arrumei o exercicio 6 da aula 03

--- aula03/exercicios/Exercicio06.java
+++ aula03/exercicios/Exercicio06.java
@@ -2,12 +2,12 @@ package exercicios;
 
 public class Exercicio06 {
     public static void main(String[] args) {
-        double nota = 2;
+        double nota = 7.5;
 
         if(nota >= 7.0){
             System.out.println("Aprovado");
         }
-        else if(nota <= 7.0){
+        else if(nota < 7.0 && nota >= 5){
             System.out.println("Exame");
         }
         else{
